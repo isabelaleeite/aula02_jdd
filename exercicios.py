6d1624e4cd52c5148d22954d9f3cc71ce724638a
Isabela.leite <isabela.leeite@hotmail.com>
2024-04-19 17:49:23 -0300

    Exercicio aula3 02

---
+++ exercicios.py
@@ -0,0 +1,8 @@
+#nome_aluno = "Gustavo@gmail.com"
+#print(nome_aluno.split("@"))
+ 
+numero_1 = int(input("Inserir um numero inteiro: "))
+numero_2 = int(input("Inserir outro numero inteiro: "))
+
+divisao = numero_1 // numero_2
+print(f"O resultado da divisão é {divisao}")
